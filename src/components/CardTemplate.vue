4ef72197ac39a132421e42d0d9b40fff129b31d0
DESKTOP-RSQRIA7\User <alexander.goyens@gmail.com>
2023-10-27 12:56:27 +0200

    cardTemplate

---
+++ src/components/CardTemplate.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="card" style="width: 18rem;">
+    <img :src="card.picture" class="card-img-top" alt="Card Image">
+    <div class="card-body" :class="{ 'show-body': isHovered }">
+      <div class="title-container">
+        <div class="box red-box">{{ card.attack }}</div>
+        <h5 class="card-title">{{ card.name }}</h5>
+        <div class="box blue-box">{{ card.defence }}</div>
+      </div>
+      <p class="card-text">{{ card.description }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    card: Object,
+  },
+  data() {
+    return {
+      isHovered: false,
+    };
+  },
+  methods: {
+    hover(hovered) {
+      this.isHovered = hovered;
+    },
+  },
+};
+</script>
--- src/views/Collection.vue
+++ src/views/Collection.vue
@@ -1,39 +1,27 @@
 <template>
   <div>
     <h1>Collection page</h1>
-    <div
-      v-for="(card, index) in filteredCards"
-      :key="index"
-      class="card"
-      :style="{ backgroundImage: `url(${card.picture})` }"
-    >
-      <div class="card-body" :class="{ 'show-body': card.isHovered }" @mouseover="card.isHovered = true" @mouseleave="card.isHovered = false">
-        <div class="title-container">
-          <div class="box red-box">{{ card.attack }}</div>
-          <h5 class="card-title">
-            {{ card.name }}
-          </h5>
-          <div class="box blue-box">{{ card.defence }}</div>
-        </div>
-        <p class="card-text">{{ card.description }}</p>
-      </div>
+    <div class="row">
+      <card-template v-for="(card, index) in filteredCards" :key="index" :card="card" class="test" />
     </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
+import CardTemplate from '../components/CardTemplate.vue';
 
 export default {
-  name: 'BootstrapCard',
+  name: "CardCollection",
+  components: {
+    CardTemplate,
+  },
   data() {
     return {
-      isHovered: false,
       cards: [],
     };
   },
   computed: {
-    // get obtained cards only
     filteredCards() {
       return this.cards.filter(card => card.obtained === 1);
     },
@@ -58,47 +46,7 @@ export default {
 </script>
 
 <style>
-  .card {
-    width: 18rem;
-    margin-bottom: 1rem;
-    background-size: cover;
-    background-position: center;
-    cursor: move;
-  }
-
-  .card-body {
-    opacity: 0;
-    transition: opacity 0.2s;
-  }
-
-  .show-body {
-    opacity: 1;
-  }
-
-  .title-container {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-  }
-
-  .box {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 25px;
-    height: 25px;
-    border-radius: 50%;
-    color: white;
-    font-weight: bold;
-  }
-
-  .red-box {
-    background-color: red;
-    margin-right: 5px;
-  }
-
-  .blue-box {
-    background-color: blue;
-    margin-left: 5px;
-  }
+.test {
+  margin-left: 10px;
+}
 </style>
