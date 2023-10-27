96f0083b9af0b1c004f9fb290030610a48a8550f
MrMojoRising777 <alexander.goyens@gmail.com>
2023-10-27 16:36:43 +0200

    template styling

--- src/components/CardTemplate.vue
+++ src/components/CardTemplate.vue
@@ -2,10 +2,10 @@
   <div class="card" style="width: 18rem;">
     <img :src="card.picture" class="card-img-top" alt="Card Image">
     <div class="card-body" :class="{ 'show-body': isHovered }">
-      <div class="title-container">
-        <div class="box red-box">{{ card.attack }}</div>
+      <div class="title-container" style="position: relative;">
+        <div class="box red-box top-left">{{ card.attack }}</div>
         <h5 class="card-title">{{ card.name }}</h5>
-        <div class="box blue-box">{{ card.defence }}</div>
+        <div class="box blue-box top-right">{{ card.defence }}</div>
       </div>
       <p class="card-text">{{ card.description }}</p>
     </div>
@@ -29,3 +29,27 @@ export default {
   },
 };
 </script>
+
+<style>
+.top-left {
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: red;
+  color: white;
+  border-radius: 50%;
+  padding: 5px;
+  font-size: 18px;
+}
+
+.top-right {
+  position: absolute;
+  top: 0;
+  right: 0;
+  background-color: blue;
+  color: white;
+  border-radius: 50%;
+  padding: 5px;
+  font-size: 18px;
+}
+</style>
