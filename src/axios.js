a631bcf93490eca679b5126fe4a90a92a7b34e93
MrMojoRising777 <alexander.goyens@gmail.com>
2023-10-31 15:37:31 +0100

    merge conflict resolve

--- src/axios.js
+++ src/axios.js
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 import axios from "axios";
 
 axios.defaults.withCredentials = true;
 axios.defaults.baseURL = "http://localhost:8000";
-=======
-import axios from 'axios';
-
-axios.defaults.withCredentials = true;
-axios.defaults.baseURL = 'http://localhost:8000';
->>>>>>> bfa41a9064ae5804644e2efeb769de501fc122f3
