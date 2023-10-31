28f8f4668c8e20582dbacd0b0a13342effe62a61
MrMojoRising777 <alexander.goyens@gmail.com>
2023-10-31 20:17:07 +0100

    csrf-token

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -1,3 +1,32 @@
+<script setup>
+import { ref } from 'vue';
+import axios from 'axios';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const form = ref({
+  name: '',
+  email: '',
+  password: '',
+  password_confirmation: ''
+});
+
+const getToken = async () => {
+  await axios.get('/sanctum/csrf-cookie');
+};
+
+const handleRegister = async () => {
+  await getToken();
+  await axios.post('/register', {
+    name: form.value.name,
+    email: form.value.email,
+    password: form.value.password,
+    password_confirmation: form.value.password_confirmation
+  });
+  router.push("/");
+}
+</script>
+
 <template>
   <div class="container mt-5">
     <div class="row">
@@ -8,19 +37,19 @@
             <form @submit.prevent="handleRegister">
               <div class="mb-3">
                 <label for="name" class="form-label">Name</label>
-                <input type="text" id="name" v-model="name" class="form-control" required>
+                <input type="text" id="name" v-model="form.name" class="form-control" required>
               </div>
               <div class="mb-3">
                 <label for="email" class="form-label">Email</label>
-                <input type="email" id="email" v-model="email" class="form-control" required>
+                <input type="email" id="email" v-model="form.email" class="form-control" required>
               </div>
               <div class="mb-3">
                 <label for="password" class="form-label">Password</label>
-                <input type="password" id="password" v-model="password" class="form-control" required>
+                <input type="password" id="password" v-model="form.password" class="form-control" required>
               </div>
               <div class="mb-3">
                 <label for="password_confirmation" class="form-label">Password Confirmation</label>
-                <input type="password" id="password_confirmation" v-model="password_confirmation" class="form-control" required>
+                <input type="password" id="password_confirmation" v-model="form.password_confirmation" class="form-control" required>
               </div>
               <div class="mb-4">
                 <button type="submit" class="btn btn-primary btn-block">Register</button>
@@ -36,13 +65,3 @@
     </div>
   </div>
 </template>
-
-<script>
-  export default {
-    
-  }
-</script>
-
-<style lang="scss" scoped>
-
-</style>
